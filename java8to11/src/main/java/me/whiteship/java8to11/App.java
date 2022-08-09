d455a7745bf8a336497f7c8811cd0f566547de80
leeheeweon <ggoddii@kaoni.com>
2022-08-10 07:39:30 +0900

    Stream 소개

--- java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,18 +1,28 @@
 package me.whiteship.java8to11;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Spliterator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        ArrayList<String> name = new ArrayList<>();
-        name.add("heeweon");
-        name.add("hee");
-        name.add("toby");
-        name.add("foo");
+        ArrayList<String> names = new ArrayList<>();
+        names.add("heeweon");
+        names.add("hee");
+        names.add("toby");
+        names.add("foo");
 
-        name.sort(String::compareToIgnoreCase);
-        name.forEach(System.out::println);
+        List<String> collect1 = names.parallelStream().map(String::toUpperCase).collect(Collectors.toList());
+        collect1.forEach(System.out::println);
 
+        List<String> collect = names.stream().map((s) -> {
+            return s.toUpperCase();
+        }).collect(Collectors.toList());
+
+        names.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
     }
 }
