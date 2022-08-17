2f52f96a5ff5fcc510ac6c3365cf885cd1becdc5
leeheeweon <ggoddii@kaoni.com>
2022-08-18 07:49:56 +0900

    Executors

--- java8to11/src/main/java/me/whiteship/java8to11/App.java
+++ java8to11/src/main/java/me/whiteship/java8to11/App.java
@@ -1,27 +1,18 @@
 package me.whiteship.java8to11;
 
-
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class App {
-    public static void main(String[] args) throws InterruptedException {
-        LocalDate now = LocalDate.now();
-        LocalDate heeweon = LocalDate.of(1994, 6, 26);
-        Period between = Period.between(now, heeweon);
-        System.out.println(between.getDays());
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            System.out.println("Thread " + Thread.currentThread().getName());
+        });
 
-        Period until = now.until(heeweon);
-        System.out.println(until);
-        System.out.println(until.get(ChronoUnit.DAYS));
-
-        LocalDateTime now1 = LocalDateTime.now();
-        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println(now1.format(DateTimeFormatter.ISO_DATE));
-
-        LocalDate parse = LocalDate.parse("06/26/1994", pattern);
-        System.out.println(parse);
-
+        executorService.shutdown();
     }
+
+
 }
